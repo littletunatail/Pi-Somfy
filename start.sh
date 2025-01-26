144f62110a991d23fbe71daa9e61a93f08fc341e
littletunatail <lotteluo.de@live.com>
2025-01-26 16:18:45 +0100

    corrected typo

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo /homw/pi/Pi-Somfy/venv/bin/python3 /home/pi/Pi-Somfy/operateShutters.py -c /home/pi/Pi-Somfy/operateShutters.conf -a -m &
+sudo /home/pi/Pi-Somfy/venv/bin/python3 /home/pi/Pi-Somfy/operateShutters.py -c /home/pi/Pi-Somfy/operateShutters.conf -a &
 fauxmo -c /home/pi/Pi-Somfy/fauxmo_config.json -vv
